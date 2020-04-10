9cc31d36d8db898c246c4716fd676027ce200e19
Jorge Herrera <jorge.herrera.tume@gmail.com>
2020-04-10 01:13:18 -0500

    estilos

--- app/src/main/java/com/example/nearbydiscosnevents/LoginActivity.java
+++ app/src/main/java/com/example/nearbydiscosnevents/LoginActivity.java
@@ -2,13 +2,54 @@ package com.example.nearbydiscosnevents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ *
+ * @author Jorge M. Herrera Tume
+ */
 
 public class LoginActivity extends AppCompatActivity {
+
+    /**
+     *
+     * @param txtRegistrar: Variable asignada par abrir activity de registro.
+     */
+    private static Button btnIngresar;
+    private static TextView txtRegistrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        txtRegistrar = findViewById(R.id.tvCreateAccountLogin);
+        btnIngresar = findViewById(R.id.btnLogin);
+
+        txtRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                abrirRegistro();
+            }
+        });
+
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+    private void abrirRegistro() {
+        Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+        LoginActivity.this.startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/nearbydiscosnevents/RegisterActivity.java
+++ app/src/main/java/com/example/nearbydiscosnevents/RegisterActivity.java
@@ -2,13 +2,43 @@ package com.example.nearbydiscosnevents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ *
+ * @author Jorge M. Herrera Tume
+ */
 
 public class RegisterActivity extends AppCompatActivity {
+
+    /**
+     *
+     * @param txtIngresar: Variable asignada par abrir activity de login.
+     */
+
+    private static TextView txtIngresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        txtIngresar = findViewById(R.id.tvGoToLogin);
+
+        txtIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirLogin();
+            }
+        });
+    }
+
+    private void abrirLogin() {
+        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+        RegisterActivity.this.startActivity(intent);
+        finish();
     }
 }
